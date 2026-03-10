2b62346a30b5e3f508129712f5517284bd254c70
trieuthikimvan-lang <trieuthikimvan@gmail.com>
2026-03-10 20:17:41 +0700

    abcd

--- WebApplication1_bai02_bainop/WebApplication1/ketnoi.cs
+++ WebApplication1_bai02_bainop/WebApplication1/ketnoi.cs
@@ -11,7 +11,7 @@ namespace WebApplication1
 {
     public partial class Ketnoi
     {
-        public SqlConnection con = new SqlConnection("Data source=.; Initial Catalog=DEMO; Integrated Security=True");
+        public SqlConnection con = new SqlConnection("Data source=ASUS\\SQLEXPRESS; Initial Catalog=DEMO; Integrated Security=True");
 
         [Obsolete]
         public string Mahoa(string str)
