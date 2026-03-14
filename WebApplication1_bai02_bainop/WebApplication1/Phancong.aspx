87df76ab0cff42a5438064fe06de53b6e4e23a5f
trieuthikimvan-lang <trieuthikimvan@gmail.com>
2026-03-14 14:04:04 +0700

    phancong

---
+++ WebApplication1_bai02_bainop/WebApplication1/Phancong.aspx
@@ -0,0 +1,108 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="PhancongGV.aspx.cs" Inherits="WebApplication1.Admin.PhancongGV" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2 class="page-title">Phân công giáo viên</h2>
+
+    <div class="form-card">
+
+        <table class="table">
+
+            <tr>
+                <td width="200">Mã GV</td>
+                <td>
+                    <asp:DropDownList ID="ddlGV" runat="server" Width="200px">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+
+            <tr>
+                <td>Mã MH</td>
+                <td>
+                    <asp:DropDownList ID="ddlMH" runat="server" Width="200px">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+
+            <tr>
+                <td>Ghi chú</td>
+                <td>
+                    <asp:TextBox ID="txtghichu" runat="server" CssClass="form-control"></asp:TextBox>
+                </td>
+            </tr>
+
+            <tr>
+                <td>Username</td>
+                <td>
+                    <asp:TextBox ID="txtusername" runat="server" CssClass="form-control"></asp:TextBox>
+                </td>
+            </tr>
+
+ <tr>
+ <td>
+
+   <asp:Button ID="btnThem" runat="server" Text="Thêm"  CssClass="btn-custom btn-add" OnClick="btnLogin_Click" />
+
+   <asp:Button ID="btnsua" runat="server" Text="Sửa" CssClass="btn-custom btn-edit" OnClick="Btnsua_Click" />
+
+ </td>
+ </tr>
+
+ </table>
+
+ </div>
+    <div class="form-card">
+
+        <table class="table">
+
+        <tr>
+
+         <td width="150">
+
+         <asp:Button ID="btntim" runat="server" Text="Tìm kiếm" CssClass="btn-custom btn-search" OnClick="btntim_Click" />
+
+                </td>
+
+                <td>
+
+                    <asp:TextBox
+                        ID="txttim" runat="server" CssClass="form-control">
+                    </asp:TextBox>
+
+                </td>
+
+            </tr>
+
+        </table>
+
+    </div>
+
+        <div class="table-card">
+
+        <h4>Danh sách phân công giáo viên</h4>
+
+        <asp:GridView
+            ID="qlpcgv"
+            runat="server"
+            CssClass="table table-bordered table-hover"
+            DataKeyNames="MaGV,MaMH,Username"
+            OnRowDeleting="qlpcgv_RowDeleting"
+            OnSelectedIndexChanged="qlpcgv_SelectedIndexChanged">
+
+            <Columns>
+
+                <asp:CommandField
+                    ShowSelectButton="True"
+                    SelectText="Chọn" />
+
+                <asp:CommandField
+                    ShowDeleteButton="True"
+                    DeleteText="Xóa" />
+
+            </Columns>
+
+        </asp:GridView>
+
+    </div>
+
+</asp:Content>
---
+++ WebApplication1_bai02_bainop/WebApplication1/Phancong.aspx.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data;
+using Microsoft.SqlServer.Server;
+
+namespace WebApplication1.Admin
+{
+
+    public partial class PhancongGV: System.Web.UI.Page
+    {
+        Ketnoi kn = new Ketnoi();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] == null)
+            {
+                Response.Redirect("~/Login.aspx");
+            }
+
+                       if (!IsPostBack)
+            {
+                hienthi();
+                loadGV();
+                loadMH();
+
+            }
+            btnsua.Enabled = false;
+        }
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string magv = ddlGV.SelectedValue;
+            string mamh = ddlMH.SelectedValue;
+            string ghichu = txtghichu.Text;
+
+            string username = "";
+            if (Session["user"] != null)
+            {
+                username = Session["user"].ToString();
+            }
+
+
+            string checkus = "select * from PhanCong where MaGV='" + magv +
+                 "' and MaMH='" + mamh +
+                 "' and Username='" + username + "'";
+            SqlDataAdapter da = new SqlDataAdapter(checkus, kn.con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            string sql = "insert into PhanCong (MaGV,MaMH,Ghichu,Username) values ('" + magv + "','" + mamh + "',N'" + ghichu + "','" + username + "')";
+            SqlCommand cmd = new SqlCommand(sql, kn.con);
+            kn.con.Open();
+            cmd.ExecuteNonQuery();
+            kn.con.Close();
+            hienthi();
+            
+
+        }
+        void hienthi()
+        {
+            if (Session["user"] != null)
+            {
+                txtusername.Text = Session["user"].ToString();
+            }
+            string sql = "select * from PhanCong";
+            SqlDataAdapter da = new SqlDataAdapter(sql, kn.con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            qlpcgv.DataSource = ds;
+            qlpcgv.DataBind();
+
+        }
+
+        protected void qlpcgv_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ddlGV.Text = qlpcgv.SelectedRow.Cells[2].Text;
+            ddlMH.Text = HttpUtility.HtmlDecode(qlpcgv.SelectedRow.Cells[3].Text);
+            txtghichu.Text = HttpUtility.HtmlDecode(qlpcgv.SelectedRow.Cells[4].Text);
+            txtusername.Text = HttpUtility.HtmlDecode(qlpcgv.SelectedRow.Cells[5].Text);
+           
+            btnThem.Enabled = false;
+            btnsua.Enabled = true;
+        }
+        protected void qlpcgv_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            string magv = qlpcgv.DataKeys[e.RowIndex].Values["MaGV"].ToString();
+            string mamh = qlpcgv.DataKeys[e.RowIndex].Values["MaMH"].ToString();
+            string username = qlpcgv.DataKeys[e.RowIndex].Values["Username"].ToString();
+
+            string sql = "delete from PhanCong where MaGV='" + magv + "' and MaMH='" + mamh + "' and Username='" + username + "' ";
+            SqlCommand cmd = new SqlCommand(sql, kn.con);
+
+            kn.con.Open();
+            cmd.ExecuteNonQuery();
+            kn.con.Close();
+
+            hienthi();
+        }
+
+        protected void Btnsua_Click(object sender, EventArgs e)
+        {
+            string magv = ddlGV.Text;
+            string mamh = ddlMH.Text;
+            string ghichu = txtghichu.Text;
+            string username = txtusername.Text;
+            string sql = "update PhanCong set Ghichu=N'" + ghichu + "' where MaGV='" + magv + "' and MaMH='" + mamh + "' and Username='" + username + "'";
+            SqlCommand cmd = new SqlCommand(sql, kn.con);
+            kn.con.Open();
+            cmd.ExecuteNonQuery();
+            kn.con.Close();
+            hienthi();
+
+            btnThem.Enabled = true;
+        
+            txtghichu.Text = "";
+            txtusername.Text = "";
+
+        }
+        void timkiem(string keywords)
+        {
+            string sql = "SELECT pc.MaGV, gv.TenGV, pc.MaMH, mh.TenMH, pc.GhiChu, pc.Username " +
+                 "FROM PhanCong pc " +
+                 "LEFT JOIN GiaoVien gv ON pc.MaGV = gv.MaGV " +
+                 "LEFT JOIN MonHoc mh ON pc.MaMH = mh.MaMH " +
+                 "WHERE pc.MaGV LIKE '%" + keywords + "%'";
+
+            SqlDataAdapter da = new SqlDataAdapter(sql, kn.con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            qlpcgv.DataSource = ds;
+            qlpcgv.DataBind();
+        }
+
+
+        protected void btntim_Click(object sender, EventArgs e)
+        {
+            string magv = txttim.Text;
+            timkiem(magv);
+        }
+
+        void loadGV()
+        {
+            string sql = "select MaGV from GiaoVien";
+            SqlDataAdapter da = new SqlDataAdapter(sql, kn.con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            ddlGV.DataSource = dt;
+            ddlGV.DataTextField = "MaGV";   
+            ddlGV.DataValueField = "MaGV";   
+            ddlGV.DataBind();
+        }
+
+        void loadMH()
+        {
+            string sql = "select MaMH from MonHoc";
+            SqlDataAdapter da = new SqlDataAdapter(sql, kn.con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            ddlMH.DataSource = dt;
+            ddlMH.DataTextField = "MaMH";  
+            ddlMH.DataValueField = "MaMH";  
+            ddlMH.DataBind();
+        }
+    }
+}
