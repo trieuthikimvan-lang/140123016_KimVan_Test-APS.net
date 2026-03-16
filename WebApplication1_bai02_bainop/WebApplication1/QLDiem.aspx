f1600f3c99bd188e83e3e8ea75f0f784865baba3
trieuthikimvan-lang <trieuthikimvan@gmail.com>
2026-03-16 20:37:38 +0700

    qldiem

---
+++ WebApplication1_bai02_bainop/WebApplication1/QLDiem.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="QLDiem.aspx.cs" Inherits="WebApplication1.Admin.QLDiem" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+<h2>Quản lý điểm học viên</h2>
+
+<table>
+
+<tr>
+<td>Mã HV</td>
+<td>
+<asp:DropDownList ID="ddlHV" runat="server"></asp:DropDownList>
+</td>
+</tr>
+
+<tr>
+<td>Mã MH</td>
+<td>
+<asp:DropDownList ID="ddlMH" runat="server"></asp:DropDownList>
+</td>
+</tr>
+
+<tr>
+<td>Điểm</td>
+<td>
+<asp:TextBox ID="txtdiem" runat="server"></asp:TextBox>
+</td>
+</tr>
+
+<tr>
+<td>Username</td>
+<td>
+<asp:TextBox ID="txtusername" runat="server" ReadOnly="true"></asp:TextBox>
+</td>
+</tr>
+
+<tr>
+<td></td>
+<td>
+<asp:Button ID="btnThem" runat="server" Text="Thêm" OnClick="btnThem_Click"/>
+<asp:Button ID="btnSua" runat="server" Text="Sửa" OnClick="btnSua_Click"/>
+</td>
+</tr>
+
+</table>
+
+<br />
+
+Tìm kiếm
+<asp:TextBox ID="txttim" runat="server"></asp:TextBox>
+<asp:Button ID="btntim" runat="server" Text="Tìm" OnClick="btntim_Click"/>
+
+<br /><br />
+
+<h3>Danh sách điểm</h3>
+
+<asp:GridView ID="gvDiem" runat="server"
+AutoGenerateColumns="False"
+OnSelectedIndexChanged="gvDiem_SelectedIndexChanged"
+OnRowDeleting="gvDiem_RowDeleting"
+DataKeyNames="MaHV,MaMH,Username">
+
+<Columns>
+
+<asp:CommandField ShowSelectButton="True"/>
+<asp:CommandField ShowDeleteButton="True"/>
+
+<asp:BoundField DataField="MaHV" HeaderText="MaHV"/>
+<asp:BoundField DataField="MaMH" HeaderText="MaMH"/>
+<asp:BoundField DataField="Diem" HeaderText="Diem"/>
+<asp:BoundField DataField="Username" HeaderText="Username"/>
+
+</Columns>
+
+</asp:GridView>
+
+</asp:Content>
---
+++ WebApplication1_bai02_bainop/WebApplication1/QLDiem.aspx.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data;
+using Microsoft.SqlServer.Server;
+
+namespace WebApplication1.Admin
+{
+    public partial class QLDiem : System.Web.UI.Page
+    {
+        Ketnoi kn = new Ketnoi();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+
+            if (!IsPostBack)
+            {
+                hienthi();
+                loadHV();
+                loadMH();
+            }
+
+            txtusername.Text = Session["user"].ToString();
+        }
+
+        void hienthi()
+        {
+            string sql = "select * from KetQua";
+            SqlDataAdapter da = new SqlDataAdapter(sql, kn.con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            gvDiem.DataSource = dt;
+            gvDiem.DataBind();
+        }
+
+        protected void btnThem_Click(object sender, EventArgs e)
+        {
+            string mahv = ddlHV.SelectedValue;
+            string mamh = ddlMH.SelectedValue;
+            string diem = txtdiem.Text;
+            string username = Session["user"].ToString();
+            if (Session["user"] != null)
+            {
+                username = Session["user"].ToString();
+            }
+            string checkus = "select * from Ketqua where MaHV='" + mahv +
+                "' and MaMH='" + mamh +
+                "' and Username='" + username + "'";
+            SqlDataAdapter da = new SqlDataAdapter(checkus, kn.con);
+            DataTable dt = new DataTable();
+            if (dt.Rows.Count == 0)
+            {
+                string sql = "insert into KetQua values('" + mahv + "','" + mamh + "','" + diem + "','" + username + "')";
+                SqlCommand cmd = new SqlCommand(sql, kn.con);
+
+                kn.con.Open();
+                cmd.ExecuteNonQuery();
+                kn.con.Close();
+            }
+
+            hienthi();
+        }
+
+        protected void btnSua_Click(object sender, EventArgs e)
+        {
+            string mahv = ddlHV.SelectedValue;
+            string mamh = ddlMH.SelectedValue;
+            string diem = txtdiem.Text;
+            string username = txtusername.Text;
+
+            string sql = "update KetQua set Diem='" + diem + "' where MaHV='" + mahv + "' and MaMH='" + mamh + "' and Username='" + username + "'";
+
+            SqlCommand cmd = new SqlCommand(sql, kn.con);
+
+            kn.con.Open();
+            cmd.ExecuteNonQuery();
+            kn.con.Close();
+
+            hienthi();
+        }
+
+        protected void gvDiem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ddlHV.Text = gvDiem.SelectedRow.Cells[2].Text;
+            ddlMH.Text = gvDiem.SelectedRow.Cells[3].Text;
+            txtdiem.Text = gvDiem.SelectedRow.Cells[4].Text;
+            txtusername.Text = gvDiem.SelectedRow.Cells[5].Text;
+
+            btnThem.Enabled = false;
+        }
+
+        protected void gvDiem_RowDeleting(object sender, System.Web.UI.WebControls.GridViewDeleteEventArgs e)
+        {
+            string mahv = gvDiem.DataKeys[e.RowIndex].Values["MaHV"].ToString();
+            string mamh = gvDiem.DataKeys[e.RowIndex].Values["MaMH"].ToString();
+            string username = gvDiem.DataKeys[e.RowIndex].Values["Username"].ToString();
+
+            string sql = "delete from KetQua where MaHV='" + mahv + "' and MaMH='" + mamh + "' and Username='" + username + "'";
+
+            SqlCommand cmd = new SqlCommand(sql, kn.con);
+
+            kn.con.Open();
+            cmd.ExecuteNonQuery();
+            kn.con.Close();
+
+            hienthi();
+        }
+
+        void loadHV()
+        {
+            string sql = "select MaHV from HocVien";
+            SqlDataAdapter da = new SqlDataAdapter(sql, kn.con);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            ddlHV.DataSource = dt;
+            ddlHV.DataTextField = "MaHV";
+            ddlHV.DataValueField = "MaHV";
+            ddlHV.DataBind();
+        }
+
+        void loadMH()
+        {
+            string sql = "select MaMH from MonHoc";
+            SqlDataAdapter da = new SqlDataAdapter(sql, kn.con);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            ddlMH.DataSource = dt;
+            ddlMH.DataTextField = "MaMH";
+            ddlMH.DataValueField = "MaMH";
+            ddlMH.DataBind();
+        }
+
+        void timkiem(string keyword)
+        {
+            string sql = "select * from KetQua where MaHV like '%" + keyword + "%'";
+
+            SqlDataAdapter da = new SqlDataAdapter(sql, kn.con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            gvDiem.DataSource = ds;
+            gvDiem.DataBind();
+        }
+
+        protected void btntim_Click(object sender, EventArgs e)
+        {
+            timkiem(txttim.Text);
+        }
+    }
+}
