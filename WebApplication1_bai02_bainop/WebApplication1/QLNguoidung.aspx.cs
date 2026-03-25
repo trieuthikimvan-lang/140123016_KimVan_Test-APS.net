6984ab8a6af500de40cea546136a17f5bb3de7c3
trieuthikimvan-lang <trieuthikimvan@gmail.com>
2026-03-25 21:50:00 +0700

    bai02bainop

---
+++ WebApplication1_bai02_bainop/WebApplication1/QLNguoidung.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class QLNguoidung : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["user"] != null)
+                {
+                    if (Session["role"].ToString() != "1")
+                        Response.Redirect("Default.aspx");
+                }
+                else
+                {
+                    Response.Redirect("Default.aspx");
+                }
+            }
+
+        }
+    }
+}
